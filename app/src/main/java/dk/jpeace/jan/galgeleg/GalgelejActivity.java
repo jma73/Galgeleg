cdfd5258032bd7c5918a0e664d69fec3422d1b2c
jan <jma73@hotmail.com>
2015-09-12 23:54:48 +0200

    tilføjet flere ting...

--- app/src/main/java/dk/jpeace/jan/galgeleg/GalgelejActivity.java
+++ app/src/main/java/dk/jpeace/jan/galgeleg/GalgelejActivity.java
@@ -2,20 +2,29 @@ package dk.jpeace.jan.galgeleg;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GalgelejActivity extends Activity implements OnClickListener {
 
-    Button buttonTest;
+    Galgelogik gl;
+
+    private Button buttonSpil;
     private TextView editviewTest;
     private ImageView imageView1;
+    private Button buttonGæt;
+    private EditText editTextBogstav;
+    private TextView textView2Show;
+    private TextView textViewDebug;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +32,17 @@ public class GalgelejActivity extends Activity implements OnClickListener {
         setContentView(R.layout.activity_galgelej);
 
         editviewTest = (TextView)findViewById(R.id.textView);
-        buttonTest = (Button) findViewById(R.id.buttonTest);
-        buttonTest.setOnClickListener(this);
+        textView2Show = (TextView)findViewById(R.id.textView2);
+        textViewDebug = (TextView)findViewById(R.id.textViewDebug);
+        editTextBogstav = (EditText)findViewById(R.id.editTextBogstav);
+        buttonGæt = (Button) findViewById(R.id.buttonGæt);
+        buttonSpil = (Button) findViewById(R.id.buttonSpil);
+        buttonGæt.setOnClickListener(this);
+        buttonSpil.setOnClickListener(this);
 
         imageView1 =  (ImageView)findViewById(R.id.imageViewet);
         imageView1.setImageResource(R.drawable.forkert6);
+        gl = new Galgelogik();
 
     }
 
@@ -55,20 +70,38 @@ public class GalgelejActivity extends Activity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-        if(v==buttonTest)
+        if(v== buttonSpil)
             TestGalgelegLogik();
+        if(v==buttonGæt)
+            BogstavTest(editTextBogstav.getText());
+    }
+
+    private void BogstavTest(Editable text) {
+        String text1 = "Du tastede: " + text;
+        Toast.makeText(this, text1, Toast.LENGTH_SHORT);
+
+        textViewDebug.setText(text1);
+
+        if(text.length() != 1)
+        {
+            Toast.makeText(this, "Du skal taste eet bogstav!", Toast.LENGTH_SHORT);
+
+        }
+        else
+        {
+            gl.gætBogstav(text.toString());
+        }
     }
 
     private void TestGalgelegLogik() {
 
 
-        Galgelogik gl = new Galgelogik();
         gl.nulstil();   // når spillet starter
 
 
         String ordet = gl.getOrdet();
 
-        editviewTest.setText(ordet);
+        textView2Show.setText(ordet);
 
     }
 
