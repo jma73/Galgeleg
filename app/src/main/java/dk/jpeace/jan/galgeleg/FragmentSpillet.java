990560ef382e9c2256bd576c42a83ec41aa9e227
jma73 <jma73@hotmail.com>
2015-09-29 22:23:07 +0200

    virker. men ser ikke pænt ud...

--- app/src/main/java/dk/jpeace/jan/galgeleg/FragmentSpillet.java
+++ app/src/main/java/dk/jpeace/jan/galgeleg/FragmentSpillet.java
@@ -1,17 +1,23 @@
 package dk.jpeace.jan.galgeleg;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.Editable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -21,8 +27,13 @@ public class FragmentSpillet extends Fragment implements View.OnClickListener {
 
     Date startTime;
     private Button buttonGætBogstav;
-    private TextView TextViewTest1;
-
+    private TextView textViewTest1;
+    private TextView TextViewFSordVisning;
+    private ImageView imageViewetFS;
+    private EditText editTextBogstav;
+    private TextView textViewFSheader;
+    private TextView textViewBrugteBogst;
+    private TextView textViewFSbottom;
 
 
     public FragmentSpillet() {
@@ -41,8 +52,15 @@ public class FragmentSpillet extends Fragment implements View.OnClickListener {
         buttonGætBogstav = (Button) rod.findViewById(R.id.buttonGætBogstav);
         buttonGætBogstav.setOnClickListener(this);
 
-        // TextViewTest1
-        TextViewTest1 = (TextView) rod.findViewById(R.id.TextViewFragSpilTest1);
+        // textViewTest1
+        textViewTest1 = (TextView) rod.findViewById(R.id.TextViewFragSpilTest1);
+        TextViewFSordVisning = (TextView) rod.findViewById(R.id.TextViewFSordVisning);
+        textViewFSheader = (TextView) rod.findViewById(R.id.textViewFSheader);
+        textViewBrugteBogst = (TextView) rod.findViewById(R.id.textViewFSbrugteBogstaver);
+        textViewFSbottom = (TextView) rod.findViewById(R.id.textViewFSbottom);
+        editTextBogstav = (EditText) rod.findViewById(R.id.editTextBogstav);
+
+        imageViewetFS = (ImageView) rod.findViewById(R.id.imageViewetFS);
 
 
         StartSpil();        // pt. bare til test...
@@ -58,8 +76,102 @@ public class FragmentSpillet extends Fragment implements View.OnClickListener {
         if(view == buttonGætBogstav)
         {
             ForsideActivity.galgelogik.getOrdet();
+            textViewTest1.setText("så vi igang - with Fragmentzzzz");
 
-            TextViewTest1.setText("så vi igang - with Fragmentzzzz");
+            BogstavTest(editTextBogstav.getText());
+        }
+
+    }
+
+    private void BogstavTest(Editable text) {
+        String text1 = "Du tastede: " + text;
+        //Toast.makeText(this, text1, Toast.LENGTH_SHORT).show();
+
+        //textViewDebug.setText(text1);
+
+        if(text.length() != 1)
+        {
+            imageViewetFS.setImageResource(R.drawable.forkert3);
+
+            Log.d("jj", "det indtastede passer ikke. " + text1);
+//            Toast.makeText(FragmentSpillet.this, "Fejl: Du skal taste præcis eet bogstav!", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+
+            ForsideActivity.galgelogik.gætBogstav(text.toString());
+            String syngligtOrd = ForsideActivity.galgelogik.getSynligtOrd();
+            TextViewFSordVisning.setText(syngligtOrd);
+
+            VisBrugteBogstaver();
+
+            editTextBogstav.setText("");
+        }
+
+        OpdaterGalgeBillede();
+        SpilStatus();
+
+    }
+
+    private void VisBrugteBogstaver() {
+        String display = "";
+        ArrayList<String> bb = ForsideActivity.galgelogik.getBrugteBogstaver();
+        for (String ii:bb  ) {
+            display += ii;
+        }
+        textViewBrugteBogst.setText(display);
+    }
+
+    private void SpilStatus() {
+
+        Date spilSlut = new Date();
+        long spilleTid = GalgelejActivity.getDateDiff(startTime, spilSlut, TimeUnit.SECONDS);
+
+
+        if(ForsideActivity.galgelogik.erSpilletSlut()) {
+
+
+//            Intent spilletErSlutIntent = new Intent(getApplicationContext(), SpilSlutActivity.class);
+
+            if(ForsideActivity.galgelogik.erSpilletTabt())
+            {
+                String statusText = "Spillet er tabt!!!";
+                textViewTest1.setText(statusText);
+                textViewTest1.setTextColor(Color.RED);
+
+                Toast.makeText(getActivity(), statusText, Toast.LENGTH_LONG).show();
+                Log.d("jj", "spillet er tabt!");
+                buttonGætBogstav.setEnabled(false);
+
+//                spilletErSlutIntent.putExtra(EXTRA_MESSAGE, statusText);
+//                spilletErSlutIntent.putExtra(ER_SPIL_VUNDET, "false");
+//                spilletErSlutIntent.putExtra(SPILLE_TID, spilleTid);
+//                startActivity(spilletErSlutIntent);
+            }
+            else if(ForsideActivity.galgelogik.erSpilletVundet())
+            {
+                String statusText = "Spillet er vundet!!!";
+                textViewTest1.setText(statusText);
+                textViewTest1.setTextColor(Color.GREEN);
+                Toast.makeText(getActivity(), statusText, Toast.LENGTH_LONG).show();
+                Log.d("jj", statusText);
+                buttonGætBogstav.setEnabled(false);
+
+//                spilletErSlutIntent.putExtra(EXTRA_MESSAGE, statusText);
+//                spilletErSlutIntent.putExtra(ER_SPIL_VUNDET, "true");
+//                spilletErSlutIntent.putExtra(SPILLE_TID, spilleTid);
+
+//                startActivity(spilletErSlutIntent);
+//                this.finish();
+
+
+            }
+        }
+        else
+        {
+            textViewFSbottom.setText("Tid: " + spilleTid + " sekunder");
         }
     }
 
@@ -69,15 +181,50 @@ public class FragmentSpillet extends Fragment implements View.OnClickListener {
 
         ForsideActivity.galgelogik.nulstil();   // når spillet starter
         String ordet = ForsideActivity.galgelogik.getOrdet();
-        TextViewTest1.setText(TextViewTest1.getText() + "--> " + ordet);
+        textViewTest1.setText(textViewTest1.getText() + "--> " + ordet);
         //Toast.makeText(this, "Spillet er startet!!!", Toast.LENGTH_LONG).show();
 
-//        imageView1.setImageResource(R.drawable.galge);
+        TextViewFSordVisning.setText(ForsideActivity.galgelogik.getSynligtOrd());
+
+
+//        imageViewetFS.setImageResource(R.drawable.galge);
 //        buttonSpil.setText("Nyt spil");
 //        buttonGæt.setEnabled(true);
 //        textViewFinishedStatus.setText("Spillet er i gang.");
 //
 //        textViewBrugteBogst.setText("");
 //        textViewSynligtOrd.setText(ForsideActivity.galgelogik.getSynligtOrd());
+    }
+
+    private void OpdaterGalgeBillede()
+    {
+        int antalForkerte = ForsideActivity.galgelogik.getAntalForkerteBogstaver();
+        Log.v("jj", (String.valueOf(antalForkerte)));
+        switch (antalForkerte)
+        {
+            case 0:
+                imageViewetFS.setImageResource(R.drawable.galge);
+                break;
+            case 1:
+                imageViewetFS.setImageResource(R.drawable.forkert1);
+                break;
+            case 2:
+                imageViewetFS.setImageResource(R.drawable.forkert2);
+                break;
+            case 3:
+                imageViewetFS.setImageResource(R.drawable.forkert3);
+                break;
+            case 4:
+                imageViewetFS.setImageResource(R.drawable.forkert4);
+                break;
+            case 5:
+                imageViewetFS.setImageResource(R.drawable.forkert5);
+                break;
+            case 6:
+                imageViewetFS.setImageResource(R.drawable.forkert6);
+                break;
+            default:
+                imageViewetFS.setImageResource(R.drawable.forkert6);
+        }
     }
 }
