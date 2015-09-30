3b737a929afcc4aba80c98a807d6aead67745081
jma <jma73@hotmail.com>
2015-09-30 10:23:22 +0200

    så kom spilslut fragmentet også med... så når jeg ikke mere... inden aflevering..

--- app/src/main/java/dk/jpeace/jan/galgeleg/FragmentSpilslut.java
+++ app/src/main/java/dk/jpeace/jan/galgeleg/FragmentSpilslut.java
@@ -13,12 +13,7 @@ import android.widget.TextView;
 
 
 public class FragmentSpilslut extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    // private static final String ER_SPIL_VUNDET = "false";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private boolean erSpilletVundet;
     private String spilleTid;
     private TextView textViewSpilSlutTid;
